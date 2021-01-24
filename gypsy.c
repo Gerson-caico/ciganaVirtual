ac93d6f1ec3c2094cde5e369108efa295d840641
Gerson <55540883+Gerson-caico@users.noreply.github.com>
2021-01-23 22:50:46 -0300

    Add files via upload
    Este programa pretende fazer previsões astrológicas a partir dos dados do usuário. Mas somente para quem acredita nos horóscopos do Zodíaco....hehehe

---
+++ gypsy.c
@@ -0,0 +1,65 @@
+/*
+Universidade Federal do Rio Grande do Norte                 
+Centro de Ensino Superior do Seridó - CERES     
+Projeto Sistema de Gestão de Sortilégios                   
+Desenvolvido por @gerson_souza - verão/2021 
+*/                 
+
+#include <stdio.h>
+#include <string.h>
+
+void telaInicial(void);
+
+int main(void) {
+    telaInicial();
+    return 0;
+
+}
+
+
+void telaInicial(void) {
+    printf("%s"," \n");
+    printf("%s"," \n");
+
+    printf("%s","***********************************************************************\n");
+    printf("%s","******** = = = = = Sistema de Gestão de Sortilégios = = = = = *********\n");
+    printf("%s","***********************************************************************\n");
+    
+    printf("%s"," \n");
+    printf("%s"," \n");
+    
+    printf("%s","Apesar de não possuir fundamentação científica comprovada, a Astrologia continua a atrair a atenção de milhares de pessoas ao redor do mundo. Não é raro encontrar pessoas dos mais variados níveis culturais que incluem, como parte dos seus hábitos cotidianos, a consulta a um ou mais  mecanismos de adivinhação, mesmo que o façam apenas por diversão. Jornais, revistas e sites na internet continuam a publicar diariamente horóscopos e demais adivinhações baseadas na Astrologia. O presente projeto tem como objetivo desenvolver um programa de computador que simule a realização de previsões astrológicas, baseadas em dados reais do usuário, que serão efetuadas uma espécie de Cigana Virtual. \n");
+    
+    printf("%s"," \n");
+    printf("%s"," \n");
+
+    printf("%s","Veja os 12 signos do Zodíaco:\n");
+    printf("%s"," \n");
+    printf("%s","- ÁRIES: de 21/Março a 19/Abril\n");
+    printf("%s"," \n");
+    printf("%s","- TOURO: de 20/Abril a 20/Maio\n");
+    printf("%s"," \n");
+    printf("%s","- GÊMEOS: de 21/Maio a 21/Junho\n");
+    printf("%s"," \n");
+    printf("%s","- CÂNCER: de 22/Junho a 22/Julho\n");
+    printf("%s"," \n");
+    printf("%s","- LEÃO: de 23/Julho a 22/Agosto\n");
+    printf("%s"," \n");
+    printf("%s","- VIRGEM: de 23/Agosto a 22/Setembro\n");
+    printf("%s"," \n");
+    printf("%s","- LIBRA: de 23/Setembro a 22/Outubro\n");
+    printf("%s"," \n");
+    printf("%s","- ESCORPIÃO: de 23/Outubro a 21/Novembro\n");
+    printf("%s"," \n");
+    printf("%s","- SAGITÁRIO: de 22/Novembro a 21/Dezembro\n");
+    printf("%s"," \n");
+    printf("%s","- CAPRICÓRNIO: de 22/Dezembro a 19/Janeiro\n");
+    printf("%s"," \n");
+    printf("%s","- AQUÁRIO: de 20/Janeiro a 18/Fevereiro\n");
+    printf("%s"," \n");
+    printf("%s","- PEIXES: de 19/Fevereiro a 20/Março\n");
+    printf("%s"," \n");
+
+    
+    
+}
